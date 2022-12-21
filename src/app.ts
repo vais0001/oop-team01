4368de185b12be3720fe7aab19855788e9cab95d
netas369 <neve0023@hz.nl>
2022-12-21 10:14:30 +0100

    version 0.01
    Added NightmareStreet.js class to act as a main class

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import DareDriver from './DareDriver.js';
+import NightmareStreet from './NigthmareStreet.js';
 import { GameLoop } from './GameLoop.js';
 
-const game = new DareDriver(document.getElementById('game') as HTMLCanvasElement);
+const game = new NightmareStreet(document.getElementById('game') as HTMLCanvasElement);
 
 const gameLoop = new GameLoop(game);
 window.addEventListener('load', () => {
