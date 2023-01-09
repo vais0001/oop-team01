961da5bd48f43f637f571d0fb39ea8c1d7cb18ae
tomasx420 <tomk0002@hz.nl>
2023-01-09 16:33:00 +0200

    0.29.2
    working credit and start buttons

--- src/StartScene.ts
+++ src/StartScene.ts
@@ -2,9 +2,12 @@ import Bedroom from './Bedroom.js';
 import CanvasUtil from './CanvasUtil.js';
 import Scene from './Scene.js';
 import KeyListener from './KeyListener.js';
+import CreditScene from './CreditScene.js';
 
 export default class StartScene extends Scene {
   private starting: boolean;
+
+  private creditscene: boolean;
 
   private button: HTMLImageElement;
 
@@ -12,15 +15,22 @@ export default class StartScene extends Scene {
     super(MaxX, MaxY);
     this.image = CanvasUtil.loadNewImage('./placeholders/start800.png');
     this.starting = false;
+    this.creditscene = false;
     this.button = CanvasUtil.loadNewImage('./placeholders/start_button.png');
   }
 
   public processInput(KeyListener: KeyListener): void {
-    return null;
+    if (KeyListener.keyPressed('KeyS')) {
+      this.starting = true;
+    }
+    if (KeyListener.keyPressed('KeyC')) {
+      this.creditscene = true;
+    }
   }
 
   public update(elapsed: number): Scene {
     if (this.starting) return new Bedroom(this.maxX, this.maxY);
+    if (this.creditscene) return new CreditScene(this.maxX, this.maxY);
     return null;
   }
 
@@ -29,9 +39,7 @@ export default class StartScene extends Scene {
     CanvasUtil.drawImage(canvas, this.image, 50, 0);
     CanvasUtil.drawImage(canvas, this.button, 460, 270);
     CanvasUtil.drawImage(canvas, this.button, 460, 400);
-    CanvasUtil.drawImage(canvas, this.button, 460, 530);
     CanvasUtil.writeTextToCanvas(canvas, '[S] to start', canvas.width / 2, canvas.height / 2 - 35, 'center', 'Kongtext', 35, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[I] for Controls', canvas.width / 2, canvas.height / 2 + 95, 'center', 'Kongtext', 35, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[C] for Credits', canvas.width / 2, canvas.height / 2 + 225, 'center', 'Kongtext', 35, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[C] for Credits', canvas.width / 2, canvas.height / 2 + 95, 'center', 'Kongtext', 35, 'black');
   }
 }
