03d3db6b83c691c01348f625626e105640c8559c
netas369 <neve0023@hz.nl>
2023-01-09 13:29:34 +0100

    0.27
    added timmy

--- src/Bedroom.ts
+++ src/Bedroom.ts
@@ -1,29 +1,38 @@
 import CanvasUtil from './CanvasUtil.js';
 import KeyListener from './KeyListener.js';
+import Player from './Player.js';
 import Scene from './Scene.js';
 
 export default class Bedroom extends Scene {
-  private background: HTMLImageElement;
 
   private starting: boolean;
 
+  private player: Player;
+
   public constructor(MaxX: number, MaxY: number) {
     super(MaxX, MaxY);
-    this.background = CanvasUtil.loadNewImage('./placeholders/timmysroom.png');
+    this.image = CanvasUtil.loadNewImage('./placeholders/timmysroom.png');
     this.starting = false;
+    this.player = new Player(window.innerWidth, window.innerHeight);
   }
 
   public processInput(keyListener: KeyListener): void {
     if (keyListener.keyPressed(KeyListener.KEY_S)) this.starting = true;
+    if (keyListener.isKeyDown(KeyListener.KEY_LEFT)) this.player.move(0);
+    if (keyListener.isKeyDown(KeyListener.KEY_UP)) this.player.move(1)
+    if (keyListener.isKeyDown(KeyListener.KEY_RIGHT)) this.player.move(2)
+    if (keyListener.isKeyDown(KeyListener.KEY_DOWN)) this.player.move(3)
   }
 
   public update(elapsed: number): Scene {
+
     return null;
   }
 
   public render(canvas: HTMLCanvasElement): void {
     CanvasUtil.clearCanvas(canvas);
     CanvasUtil.fillCanvas(canvas, 'black');
-    CanvasUtil.drawImage(canvas, this.background, 0, 0);
+    CanvasUtil.drawImage(canvas, this.image, 0, 0);
+    this.player.render(canvas)
   }
 }
--- src/Player.ts
+++ src/Player.ts
@@ -8,6 +8,24 @@ export default class Player extends Drawable {
   constructor(maxX: number, maxY: number) {
     super();
     this.image = CanvasUtil.loadNewImage('./placeholders/timmy_00000.png')
+    this.posX = maxX  / 2;
+    this.posY = maxY / 2;
+    this.speed = 5;
+  }
+
+  public move(direction: number): void {
+    if (direction == 0) {
+      this.posX -= this.speed;
+    }
+    if (direction == 1){
+      this.posY -= this.speed;
+    }
+    if (direction == 2) {
+      this.posX += this.speed;
+    }
+    if (direction == 3) {
+      this.posY += this.speed;
+    }
   }
 
 }
