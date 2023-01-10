7be9d376bed66ebd19c6f9ef4bafcbf9948476bc
tomasx420 <tomk0002@hz.nl>
2023-01-10 16:52:34 +0200

    0.5.4
    fixed the loading bar

--- src/LoadingScenes/LoadingScene.ts
+++ src/LoadingScenes/LoadingScene.ts
@@ -6,26 +6,49 @@ import Scene from "../Scene.js";
 export default class LoadingScene extends Scene {
   private loadingBar: number;
 
+  private realisticPause: number;
+
+  private continue: boolean;
+
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     this.loadingBar = 0;
     this.image = CanvasUtil.loadNewImage('./placeholders/loading_screen_controls.png');
+    this.realisticPause = 50;
+    this.continue = false;
   }
 
   public processInput(keyListener: KeyListener): void {
+    if (this.loadingBar === 1220 && keyListener.keyPressed('Space')) {
+      this.continue = true;
+    }
     return null;
   }
 
   public update(elapsed: number): Scene {
-    this.loadingBar += elapsed * 0.15;
-    if (this.loadingBar > 500) return new Bedroom(this.maxX, this.maxY, 0);
+    const randomPause: number = Math.floor(Math.random() * 1220) + 100;
+    if (this.realisticPause === 50 || this.realisticPause < 0) {
+      this.loadingBar += elapsed * 5; //for final 0.3
+    }
+    if (this.loadingBar > randomPause) {
+      this.realisticPause -= elapsed;
+      console.log(this.realisticPause);
+    }
+    if (this.loadingBar > 1220) {
+      this.loadingBar = 1220;
+    }
+
+    if (this.continue) return new Bedroom(this.maxX, this.maxY, 0);
     return null;
   }
 
   public render(canvas: HTMLCanvasElement): void {
-    CanvasUtil.fillCanvas(canvas, 'black');
-    CanvasUtil.drawImage(canvas, this.image, 50, -20);
-    CanvasUtil.drawRectangle(canvas, 100, 500, 300, 30, 'white');
-    CanvasUtil.fillRectangle(canvas, 100, 500, this.loadingBar, 30, 'white');
+    CanvasUtil.fillCanvas(canvas, 'white');
+    CanvasUtil.drawImage(canvas, this.image, this.dimensionsX, this.dimensionsY);
+    CanvasUtil.drawRectangle(canvas, this.dimensionsX + 100, this.dimensionsY + 100, 1220, 30, 'white');
+    if (this.loadingBar === 1220) {
+      CanvasUtil.writeTextToCanvas(canvas, 'Press [SPACE] to continue', canvas.width / 2, this.dimensionsY + 600, 'center', 'Arial', 40, 'White');
+    }
+    CanvasUtil.fillRectangle(canvas, this.dimensionsX + 100, this.dimensionsY + 100, this.loadingBar, 30, 'white');
   }
 }
