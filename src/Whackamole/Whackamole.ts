3119e308fae8ff7ae5921cb79fec72b77ee50c1c
netas369 <neve0023@hz.nl>
2023-01-10 12:35:17 +0100

    Update Whackamole.ts

--- src/Whackamole/Whackamole.ts
+++ src/Whackamole/Whackamole.ts
@@ -6,6 +6,7 @@ export default class Whackamole extends Scene {
 
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
+    this.image = CanvasUtil.loadNewImage('./placeholders/Whackamole_00000.png')
   }
 
   public processInput(keyListener: KeyListener): void {
@@ -15,7 +16,9 @@ export default class Whackamole extends Scene {
     return null;
   }
   public render(canvas: HTMLCanvasElement): void {
-    CanvasUtil.fillCanvas(canvas, 'darkgrey')
+    CanvasUtil.clearCanvas(canvas)
+    CanvasUtil.fillCanvas(canvas, 'darkgrey');
+    CanvasUtil.drawImage(canvas, this.image, 0, 0)
   }
 
 }
