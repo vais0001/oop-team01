9287e163e5bc4ea9a5c05976774b33f99e79630a
vais0001 <vais0001@hz.nl>
2023-01-10 12:33:47 +0100

    0.4.8
    New changes to ArrowThrower

--- src/ArrowThrower/ArrowThrower.ts
+++ src/ArrowThrower/ArrowThrower.ts
@@ -1,22 +1,32 @@
 import CanvasUtil from "../CanvasUtil.js";
 import KeyListener from "../KeyListener.js";
 import Scene from "../Scene.js";
+import Player from "./Player.js";
 
 export default class ArrowThrower extends Scene {
 
+  private player: Player;
+
   public constructor (maxX: number, maxY: number) {
     super(maxX, maxY);
+    console.log(maxX);
 
+    this.player = new Player(maxX, maxY);
+    this.image = CanvasUtil.loadNewImage('../../placeholders/arrow_thrower_scene.png');
   }
 
   public processInput(keyListener: KeyListener): void {
-    throw new Error("Method not implemented.");
+    return null;
   }
+
   public update(elapsed: number): Scene {
-    throw new Error("Method not implemented.");
+    return null;
   }
   public render(canvas: HTMLCanvasElement): void {
+    CanvasUtil.clearCanvas(canvas);
     CanvasUtil.fillCanvas(canvas, 'black');
+    CanvasUtil.drawImage(canvas, this.image, 0, 0);
+    this.player.render(canvas);
   }
 
 }
