3ee48bd141416b4baef43bb741808776a9dd54d9
vais0001 <vais0001@hz.nl>
2023-01-10 13:52:47 +0100

    Fixing dimensions

--- src/ArrowThrower/Player.ts
+++ src/ArrowThrower/Player.ts
@@ -7,8 +7,22 @@ export default class Player extends Drawable {
   public constructor (maxX: number, maxY: number) {
     super();
     this.image = CanvasUtil.loadNewImage('../../placeholders/timmy_00000.png');
-    this.posX = maxX / 2;
-    this.posY = maxY - 150;
+    this.posX = (maxX / 2) + this.dimensionsX - (this.image.width / 2);
+    this.posY = maxY - 200;
     this.maxX = maxX;
+  }
+
+  public move (direction: number): void {
+      if (direction == 0) {
+        if (this.posX > this.dimensionsX) {
+          this.posX -= 5;
+        }
+      }
+
+    if (direction == 1)  {
+      if (this.posX < (1422 + this.dimensionsX - this.image.width)) {
+        this.posX += 5;
+      }
+    }
   }
 }
