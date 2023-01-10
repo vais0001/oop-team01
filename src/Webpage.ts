688141ac2b57d907bf7fccb17b242c9a9ac9e8df
tomasx420 <tomk0002@hz.nl>
2023-01-10 12:12:54 +0200

    0.4.3
    download bar in web complete

--- src/Webpage.ts
+++ src/Webpage.ts
@@ -10,12 +10,15 @@ export default class Webpage extends Scene {
 
   private loadingBar: number;
 
+  private downloadedTime: number;
+
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     this.image = CanvasUtil.loadNewImage('./placeholders/internet_browser.png');
     this.newLevel = false;
     this.downloading = false;
     this.loadingBar = 0;
+    this.downloadedTime = 1700;
   }
 
   public processInput(keyListener: KeyListener): void {
@@ -29,7 +32,11 @@ export default class Webpage extends Scene {
       this.loadingBar += elapsed * 0.5;
     }
     if (this.loadingBar > 300) {
-      this.newLevel = true;
+      this.loadingBar = 301;
+      this.downloadedTime -= elapsed;
+      if (this.downloadedTime <= 0) {
+        this.newLevel = true;
+      }
     }
     if (this.newLevel === true) {
       return new Bedroom(0, 0, 1);
@@ -45,13 +52,14 @@ export default class Webpage extends Scene {
     CanvasUtil.clearCanvas(canvas);
     CanvasUtil.fillCanvas(canvas, 'black');
     CanvasUtil.drawImage(canvas, this.image, 50, 0);
-    CanvasUtil.drawRectangle(canvas, canvas.width / 2 - 180, 600, 300, 30, 'white');
-    CanvasUtil.fillRectangle(canvas, canvas.width / 2 - 180, 600, this.loadingBar, 30, 'green');
+    CanvasUtil.drawRectangle(canvas, canvas.width / 2 - 160, 600, 300, 30, 'white');
+    CanvasUtil.fillRectangle(canvas, canvas.width / 2 - 160, 600, this.loadingBar, 30, 'green');
     if (!(this.downloading)) {
       CanvasUtil.writeTextToCanvas(canvas, 'Press [ENTER] To Download The Game', canvas.width / 2, 700, 'center', 'arial', 40, 'white');
-    } else {
-      CanvasUtil.writeTextToCanvas(canvas, 'DOWNLOADING', canvas.width / 2, 500, 'center', 'arial', 40, 'white');
+    } if (this.downloading && this.loadingBar < 300) {
+      CanvasUtil.writeTextToCanvas(canvas, 'DOWNLOADING', canvas.width / 2, 550, 'center', 'arial', 40, 'white');
+    } if (this.loadingBar > 300) {
+      CanvasUtil.writeTextToCanvas(canvas, 'DOWNLOADED', canvas.width / 2, 550, 'center', 'arial', 40, 'red');
     }
-
   }
 }
