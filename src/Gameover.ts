02edacb37fccbf8419c23a65bf653c2d5571e740
tomasx420 <tomk0002@hz.nl>
2023-01-11 14:07:19 +0200

    0.6
    gameover screen

--- src/Gameover.ts
+++ src/Gameover.ts
@@ -1,23 +1,32 @@
 import CanvasUtil from "./CanvasUtil.js";
 import KeyListener from "./KeyListener.js";
 import Scene from "./Scene.js";
+import StartScene from "./StartScene.js";
 
 export default class Gameover extends Scene {
+  private continue: boolean;
 
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     this.image = CanvasUtil.loadNewImage('./placeholders/gameover.png')
-
+    this.continue = false;
   }
 
   public processInput(keyListener: KeyListener): void {
-
+    if (keyListener.keyPressed('Space')) {
+      this.continue = true;
+    }
   }
+
   public update(elapsed: number): Scene {
+    if (this.continue) {
+      return new StartScene(window.innerWidth, window.innerHeight);
+    }
     return null;
   }
+
   public render(canvas: HTMLCanvasElement): void {
-    CanvasUtil.fillCanvas(canvas, 'black')
+    CanvasUtil.fillCanvas(canvas, '#0078d7')
     CanvasUtil.drawImage(canvas, this.image, this.dimensionsX, this.dimensionsY)
   }
 
