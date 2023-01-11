032384844fe785f2818bfbba2daa75c504dc0486
vais0001 <vais0001@hz.nl>
2023-01-11 12:59:52 +0100

    Made changes to ArrowThrower

--- src/ArrowThrower/Player.ts
+++ src/ArrowThrower/Player.ts
@@ -2,26 +2,24 @@ import CanvasUtil from "../CanvasUtil.js";
 import Drawable from "../Drawable.js";
 
 export default class Player extends Drawable {
-  private maxX: number;
 
   public constructor (maxX: number, maxY: number) {
     super();
     this.image = CanvasUtil.loadNewImage('../../placeholders/timmy_00000.png');
-    this.posX = (maxX / 2) + this.dimensionsX - (this.image.width / 2);
+    this.posX = this.dimensionsX + this.backgroundWidth - this.image.width - 50;
     this.posY = maxY - 200;
-    this.maxX = maxX;
   }
 
   public move (direction: number): void {
     if (direction == 0) {
-      if (this.posX > this.dimensionsX) {
-        this.posX -= 5;
+      if (this.posY > (0 - this.dimensionsY)) {
+        this.posY -= 5;
       }
     }
 
     if (direction == 1)  {
-      if (this.posX < (1422 + this.dimensionsX - this.image.width)) {
-        this.posX += 5;
+      if (this.posY < (this.backgroundHeight + (0 - this.dimensionsY) - this.image.height)) {
+        this.posY += 5;
       }
     }
   }
