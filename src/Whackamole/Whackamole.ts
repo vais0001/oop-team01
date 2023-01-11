75b415df252344df7a359e7afd050006bb774d2c
netas369 <neve0023@hz.nl>
2023-01-11 11:49:56 +0100

    0.57

--- src/Whackamole/Whackamole.ts
+++ src/Whackamole/Whackamole.ts
@@ -14,10 +14,16 @@ export default class Whackamole extends Scene {
 
   private lives: Lives[] = [];
 
+  private score: number;
+
+  private checkIfCorrect: number;
+
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     this.image = CanvasUtil.loadNewImage('./assets/whackamole.jpg');
     this.timeToNextVirus = 1000;
+    this.score = 0;
+    this.checkIfCorrect = 0;
     for (let i = 0; i < 150; i += 50) {
       this.lives.push(new Lives(50, 250 + i))
     }
@@ -26,6 +32,20 @@ export default class Whackamole extends Scene {
   public processInput(keyListener: KeyListener): void {
     if (keyListener.keyPressed(KeyListener.KEY_97)) {
       this.value = 1;
+      for (let i = 0; i < this.holes.length; i++) {
+        this.holes.forEach((item: Viruses) => {
+          if (item.getValue() === 1) {
+            this.checkIfCorrect = 1;
+          }
+        })
+        if (i === this.holes.length - 1 && this.checkIfCorrect === 0) {
+          this.lives.pop()
+        }
+        if (i === this.holes.length - 1&& this.checkIfCorrect === 1) {
+          this.checkIfCorrect = 0;
+        }
+      }
+    }
     if (keyListener.keyPressed(KeyListener.KEY_98)) {
       this.value = 2;
     }
@@ -51,11 +71,14 @@ export default class Whackamole extends Scene {
       this.value = 9;
     }
   }
-  }
+
   public update(elapsed: number): Scene {
+
+
     this.holes = this.holes.filter((item: Viruses) => {
       if (this.value === item.getValue()) {
         this.value = 0;
+        this.score += 1;
         return false;
       } else return true;
     })
@@ -63,7 +86,9 @@ export default class Whackamole extends Scene {
     this.timeToNextVirus -= elapsed;
     if (this.timeToNextVirus <= 0 && this.holes.length < 4) {
       this.holes.push(new Viruses());
-      this.timeToNextVirus = 1000;
+      if (this.score < 10 ) this.timeToNextVirus = 1000;
+      if (this.score > 10 && this.score < 21) this.timeToNextVirus = 800;
+      if (this.score > 20 ) this.timeToNextVirus = 500;
     }
     return null;
   }
@@ -73,6 +98,7 @@ export default class Whackamole extends Scene {
     CanvasUtil.clearCanvas(canvas)
     CanvasUtil.fillCanvas(canvas, 'black');
     CanvasUtil.drawImage(canvas, this.image, this.dimensionsX, this.dimensionsY);
+    CanvasUtil.writeTextToCanvas(canvas, `score: ${this.score}`, 10, 50, 'left', 'arial', 22, 'white')
     this.holes.forEach((item: Viruses) => {
       item.render(canvas)
     })
