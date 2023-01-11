926221e4c023d8b060e4e3122da33c570f641212
tomasx420 <tomk0002@hz.nl>
2023-01-11 14:53:44 +0200

    0.6.3
    fixed positions

--- src/Antagonist.ts
+++ src/Antagonist.ts
@@ -5,7 +5,7 @@ export default class Antagonist extends Drawable {
   public constructor(posX: number, posY: number) {
     super();
     this.posX = this.dimensionsX + 200;
-    this.posY = this.dimensionsY + 600;
+    this.posY = this.dimensionsY + 200;
     this.image = CanvasUtil.loadNewImage('./placeholders/trojan_00000.png')
   }
 }
