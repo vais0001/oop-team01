93dfb1a7bbe99616be06014559d621502021cda0
ertu8820 <akta0002@hz.nl>
2023-01-11 12:53:36 +0100

    finished text class for bedroom
    exactly what the commit says

--- src/Text.ts
+++ src/Text.ts
@@ -34,7 +34,7 @@ export default class Text extends Drawable {
     CanvasUtil.writeTextToCanvas(canvas, 'But oh no, it is too expensive. My parents will not', this.dimensionsX + 720, this.dimensionsY + 1000, 'center', 'arial', 14, 'black');
     CanvasUtil.writeTextToCanvas(canvas, 'buy it for me unless I get high grades...', this.dimensionsX + 720, this.dimensionsY + 1020, 'center', 'arial', 14, 'black');
     CanvasUtil.writeTextToCanvas(canvas, 'I want it now, but how….', this.dimensionsX + 720, this.dimensionsY + 1040, 'center', 'arial', 14, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[N] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 540, this.dimensionsY + 1020);
   }
 
@@ -43,25 +43,24 @@ export default class Text extends Drawable {
     CanvasUtil.writeTextToCanvas(canvas, 'Narrator: Timmy browses on forums to see', this.dimensionsX + 720, this.dimensionsY + 980, 'center', 'arial', 14, 'black');
     CanvasUtil.writeTextToCanvas(canvas, 'how to access this game for free.', this.dimensionsX + 720, this.dimensionsY + 1000, 'center', 'arial', 14, 'black');
     CanvasUtil.writeTextToCanvas(canvas, 'He comes across a site which promises a safe and free download.', this.dimensionsX + 720, this.dimensionsY + 1020, 'center', 'arial', 14, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[N] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
   }
 
   public textSix(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 500, this.dimensionsY + 950);
     CanvasUtil.writeTextToCanvas(canvas, 'You fell for my trap!', this.dimensionsX + 720, this.dimensionsY + 1010, 'center', 'arial', 14, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[N] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 540, this.dimensionsY + 1020);
   }
 
   public textSeven(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 500, this.dimensionsY + 950);
     CanvasUtil.writeTextToCanvas(canvas, 'Now you must take responsibility for your actions…', this.dimensionsX + 720, this.dimensionsY + 1010, 'center', 'arial', 14, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[N] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 850, this.dimensionsY + 1060, 'center', 'arial', 14, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 540, this.dimensionsY + 1020);
   }
 
   public computerPrompt(canvas: HTMLCanvasElement, image: HTMLImageElement): void {
-    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 1205, this.dimensionsY + 400);
     CanvasUtil.writeTextToCanvas(canvas, 'Press [SPACE] to open computer', this.dimensionsX + 10, this.dimensionsY + 1050, 'left', 'arial', 40, 'white');
   }
 }
