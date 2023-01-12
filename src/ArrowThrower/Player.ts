d8b5cfc4fa0febedaacabd26a4ce71de8c9f0271
ertu8820 <akta0002@hz.nl>
2023-01-12 15:18:26 +0100

    added heart powerup to arrowthrower

--- src/ArrowThrower/Player.ts
+++ src/ArrowThrower/Player.ts
@@ -1,24 +1,25 @@
 import CanvasUtil from "../CanvasUtil.js";
 import Drawable from "../Drawable.js";
 import EnemyAD from "./EnemyAD.js";
+import HeartPowerup from "./HeartPowerup.js";
 
 export default class Player extends Drawable {
 
-  public constructor (maxX: number, maxY: number) {
+  public constructor(maxX: number, maxY: number) {
     super();
     this.image = CanvasUtil.loadNewImage('../../placeholders/timmy_00000.png');
     this.posX = this.dimensionsX + this.backgroundWidth - this.image.width - 50;
     this.posY = maxY - 200;
   }
 
-  public move (direction: number): void {
+  public move(direction: number): void {
     if (direction == 0) {
       if (this.posY > this.dimensionsY + 15) {
         this.posY -= 5;
       }
     }
 
-    if (direction == 1)  {
+    if (direction == 1) {
       if (this.posY < this.dimensionsY + this.backgroundHeight - 140) {
         this.posY += 5;
       }
@@ -27,8 +28,17 @@ export default class Player extends Drawable {
 
   public isCollidingAD(ad: EnemyAD): boolean {
     return (this.posX < ad.getPosX() + ad.getWidth()
-    && this.posX + this.getWidth() > ad.getPosX()
-    && this.getPosY() < ad.getPosY() + ad.getHeight()
-    && this.getHeight() + this.posY > ad.getPosY());
-  }
+      && this.posX + this.getWidth() > ad.getPosX()
+      && this.getPosY() < ad.getPosY() + ad.getHeight()
+      && this.getHeight() + this.posY > ad.getPosY());
+  }
+
+  public isCollidingHeart(heartpowerup: HeartPowerup): boolean {
+    return (this.posX < heartpowerup.getPosX() + heartpowerup.getWidth()
+      && this.posX + this.getWidth() > heartpowerup.getPosX()
+      && this.getPosY() < heartpowerup.getPosY() + heartpowerup.getHeight()
+      && this.getHeight() + this.posY > heartpowerup.getPosY()
+    );
+    return null;
+  }
 }
