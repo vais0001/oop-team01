dbfcb4547a2b34d066f59ecdb278b2674ac87906
vais0001 <vais0001@hz.nl>
2023-01-12 14:20:14 +0100

    fixing bugs

--- src/ArrowThrower/EnemyAD2.ts
+++ src/ArrowThrower/EnemyAD2.ts
@@ -3,26 +3,15 @@ import ADbullet from "./ADbullet.js";
 import EnemyAD from "./EnemyAD.js";
 
 export default class EnemyAD1 extends EnemyAD {
-  private timeToNextBullet: number;
-
-  private adBullets: ADbullet[] = [];
 
   public constructor(maxY: number) {
     super(maxY);
-    this.timeToNextBullet = 500;
     this.image = CanvasUtil.loadNewImage('../../placeholders/adshooter_00000.png');
   }
 
   public update(elapsed: number): void {
     if (this.posX > 201 + this.dimensionsX) {
       this.posX = 200 + this.dimensionsX;
-      this.timeToNextBullet -= elapsed;
-      if (this.timeToNextBullet < 0) {
-        this.timeToNextBullet = 500;
-        this.adBullets.push(new ADbullet(this.posX, this.posY));
-        console.log(this.adBullets)
-        console.log('fire');
-      }
     } else {
       this.posX += elapsed * 0.2;
     }
