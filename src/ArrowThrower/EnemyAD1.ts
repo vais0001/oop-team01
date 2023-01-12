d5b98d4aa25a2c86d6c85eff7b7d83a2e028877a
tomasx420 <tomk0002@hz.nl>
2023-01-12 16:01:02 +0200

    0.6.9
    loading screen optimisation

--- src/ArrowThrower/EnemyAD1.ts
+++ src/ArrowThrower/EnemyAD1.ts
@@ -1,5 +1,5 @@
-import CanvasUtil from "../CanvasUtil.js";
-import EnemyAD from "./EnemyAD.js";
+import CanvasUtil from '../CanvasUtil.js';
+import EnemyAD from './EnemyAD.js';
 
 export default class EnemyAD1 extends EnemyAD {
   public constructor(maxY: number) {
@@ -7,7 +7,7 @@ export default class EnemyAD1 extends EnemyAD {
     this.image = CanvasUtil.loadNewImage('../../placeholders/AD.png');
   }
 
-  public update(elapsed: number): void {
+  public override update(elapsed: number): void {
     this.posX += elapsed * 0.2;
   }
 }
