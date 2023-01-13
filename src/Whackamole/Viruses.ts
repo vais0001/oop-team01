d9b2b9b0a57a8660802484fb975fe26aff1197d8
netas369 <neve0023@hz.nl>
2023-01-13 13:58:47 +0100

    0.7

--- src/Whackamole/Viruses.ts
+++ src/Whackamole/Viruses.ts
@@ -48,10 +48,17 @@ export default class Viruses extends Drawable {
 
   public update(elapsed: number): boolean {
     this.timeToDissapear -= elapsed;
+<<<<<<< Updated upstream
     if (this.timeToDissapear < 1500) {
       if (this.posY === 620 + this.dimensionsY) this.posY = 520;
       if (this.posY === 430 + this.dimensionsY) this.posY = 330;
       if (this.posY === 270 + this.dimensionsY) this.posY = 170;
+=======
+    if (this.timeToDissapear < 2000) {
+      if (this.posY === 620 + this.dimensionsY) this.posY = 520 + this.dimensionsY;
+      if (this.posY === 430 + this.dimensionsY) this.posY = 330 + this.dimensionsY;
+      if (this.posY === 270 + this.dimensionsY) this.posY = 170 + this.dimensionsY;
+>>>>>>> Stashed changes
       this.image = CanvasUtil.loadNewImage('./assets/wormbig.png')
     }
     if(this.timeToDissapear <= 0) {
