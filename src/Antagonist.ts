5fc83a57c1d6db1b4666f2248760734d56c97a7d
ertu8820 <akta0002@hz.nl>
2023-01-16 12:17:25 +0100

    fixing things for merging

--- src/Antagonist.ts
+++ src/Antagonist.ts
@@ -1,11 +1,42 @@
 import Drawable from './Drawable.js';
 import CanvasUtil from './CanvasUtil.js';
+import Player from './ArrowThrower/Player.js';
 
 export default class Antagonist extends Drawable {
+  public cutsceneMoveTimer: number;
+
   public constructor(posX: number, posY: number) {
     super();
     this.posX = posX + this.dimensionsX;
     this.posY = posY + this.dimensionsY;
-    this.image = CanvasUtil.loadNewImage('./assets/trojanhorse.png')
+    this.image = CanvasUtil.loadNewImage('./assets/trojanhorse.png');
+    this.cutsceneMoveTimer = 5000;
+  }
+
+  public cutsceneMovement(elapsed: number): void {
+    this.cutsceneMoveTimer -= elapsed;
+    if (this.cutsceneMoveTimer < 0) {
+      this.posX -= 0;
+    } else if (this.cutsceneMoveTimer < 2000) {
+      this.posX -= 2;
+      this.posY -= 1;
+    } else if (this.cutsceneMoveTimer < 5000) {
+      this.posY += elapsed * 0.2;
+      this.posX += elapsed * 0.5;
+    }
+  }
+
+  public moveToPlayer(player: Player) {
+    this.posX += Math.sqrt(((player.getPosX() - this.getPosX()) ** 2));
+    this.posY += Math.sqrt(((player.getPosY() - this.getPosY()) ** 2));
+  }
+
+  public cutsceneMovementAway(speedX: number, speedY: number): void {
+    this.posX += speedX;
+    this.posY += speedY;
+  }
+
+  public getCutsceneMoveTimer(): number {
+    return this.cutsceneMoveTimer;
   }
 }
---
+++ src/ArrowThrower/ThrowerText.ts
@@ -0,0 +1,58 @@
+import CanvasUtil from "../CanvasUtil.js";
+import Drawable from "../Drawable.js";
+
+export default class ThrowerText extends Drawable {
+  public constructor() {
+    super();
+  }
+
+  public textOne(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
+    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
+    CanvasUtil.writeTextToCanvas(canvas, 'Whe… Wher…. Where am I…?', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
+  }
+
+  public textTwo(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
+    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
+    CanvasUtil.writeTextToCanvas(canvas, 'I am scared…', this.dimensionsX + 720, this.dimensionsY + 650, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
+  }
+
+  public textThree(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
+    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
+    CanvasUtil.writeTextToCanvas(canvas, 'Welcome to trojan street.', this.dimensionsX + 720, this.dimensionsY + 650, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
+  }
+
+  public textFour(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
+    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
+    CanvasUtil.writeTextToCanvas(canvas, 'Have fun getting eaten by my ads!!', this.dimensionsX + 720, this.dimensionsY + 640, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
+  }
+
+  public textFive(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
+    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
+    CanvasUtil.writeTextToCanvas(canvas, 'having fun yet?', this.dimensionsX + 720, this.dimensionsY + 640, 'center', 'arial', 20, 'black');
+    CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
+  }
+
+  public textSix(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
+    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
+    CanvasUtil.writeTextToCanvas(canvas, 'Stop it!', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
+    CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
+  }
+
+  public textSeven(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
+    CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
+    CanvasUtil.writeTextToCanvas(canvas, 'I have had enough!', this.dimensionsX + 740, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
+    CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
+  }
+
+  public computerPrompt(canvas: HTMLCanvasElement, image: HTMLImageElement): void {
+    CanvasUtil.writeTextToCanvas(canvas, 'Press [SPACE] to open computer', this.dimensionsX + 50, this.dimensionsY + 740, 'left', 'arial', 40, 'white');
+  }
+}
