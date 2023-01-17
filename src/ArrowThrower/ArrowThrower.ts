2448df1bf347fd3ae6efc0967a1b5f85e8a4a708
vais0001 <vais0001@hz.nl>
2023-01-17 12:52:10 +0100

    minor change

--- src/ArrowThrower/ArrowThrower.ts
+++ src/ArrowThrower/ArrowThrower.ts
@@ -125,11 +125,10 @@ export default class ArrowThrower extends Scene {
 
         if (this.timeToNextAD > 500) {
           if (this.timeToNextAD > 700) {
-            this.timeToNextAD -= 20; // change to 10 later
+            this.timeToNextAD -= 20;
           }
         }
         this.changingTime = this.timeToNextAD;
-        console.log(this.changingTime)
       }
 
       if (this.lives.length < 3) {
