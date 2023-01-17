c398257e9ca86f74ee4fe5c22b7ba7173d4188c7
ertu8820 <akta0002@hz.nl>
2023-01-17 13:43:18 +0100

    updated gameloop, play_catch_up

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { GameLoop } from './GameLoop.js';
 
 const game = new Nightmare(document.getElementById('game') as HTMLCanvasElement);
 
-const gameLoop = new GameLoop(game);
+const gameLoop = new GameLoop(game, GameLoop.PLAY_CATCH_UP);
 window.addEventListener('load', () => {
   gameLoop.start();
 });
