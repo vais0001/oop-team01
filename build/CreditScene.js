8fb4d794f79ca90c8aa94507b771324c606033e7
ertu8820 <akta0002@hz.nl>
2023-01-18 15:48:42 +0100

    added build to commits

--- build/CreditScene.js
+++ build/CreditScene.js
@@ -22,7 +22,12 @@ export default class CreditScene extends Scene {
     render(canvas) {
         CanvasUtil.fillCanvas(canvas, 'black');
         CanvasUtil.drawImage(canvas, this.image, this.dimensionsX, this.dimensionsY);
-        CanvasUtil.writeTextToCanvas(canvas, 'Press [SPACE] to go back', canvas.width / 2 - 50, canvas.height / 2 + 270, 'center', 'Helvetica', 30, 'black');
+        CanvasUtil.writeTextToCanvas(canvas, 'BRICK LAYERS', canvas.width / 2, canvas.height / 2 - 100, 'center', 'DotGothic16', 30, 'black');
+        CanvasUtil.writeTextToCanvas(canvas, 'Netas Neverauskas', canvas.width / 2, canvas.height / 2 - 50, 'center', 'DotGothic16', 30, 'black');
+        CanvasUtil.writeTextToCanvas(canvas, 'Ertugrul Aktas', canvas.width / 2, canvas.height / 2, 'center', 'DotGothic16', 30, 'black');
+        CanvasUtil.writeTextToCanvas(canvas, 'Dominykas Vaisnoras', canvas.width / 2, canvas.height / 2 + 50, 'center', 'DotGothic16', 30, 'black');
+        CanvasUtil.writeTextToCanvas(canvas, 'Tomas Tomkevicius', canvas.width / 2, canvas.height / 2 + 100, 'center', 'DotGothic16', 30, 'black');
+        CanvasUtil.writeTextToCanvas(canvas, 'Press [SPACE] to go back', canvas.width / 2, canvas.height / 2 + 270, 'center', 'DotGothic16', 30, 'black');
     }
 }
 //# sourceMappingURL=CreditScene.js.map
