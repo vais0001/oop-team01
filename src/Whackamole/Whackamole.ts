b0d1d4485d3a3172b8705c24fecf4be5055ca146
vais0001 <vais0001@hz.nl>
2023-01-18 18:37:53 +0100

    fixing minor bug

--- src/Whackamole/Whackamole.ts
+++ src/Whackamole/Whackamole.ts
@@ -47,7 +47,7 @@ export default class Whackamole extends Scene {
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
     this.bubble = CanvasUtil.loadNewImage('./placeholders/bubble.png');
-    this.trojanHead = CanvasUtil.loadNewImage('../assets/trojanicon.png');
+    this.trojanHead = CanvasUtil.loadNewImage('./assets/trojanicon.png');
     this.player = new Player(this.dimensionsX + this.backgroundWidth - 1550, this.dimensionsY + 50);
     this.antagonist = new Antagonist(this.backgroundWidth - 1850, this.backgroundHeight - 1000);
     this.image = CanvasUtil.loadNewImage('./assets/whackamole.jpg');
