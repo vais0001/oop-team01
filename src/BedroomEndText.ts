1536255aab23260ca6de743c5425ef7e42df11d3
ertu8820 <akta0002@hz.nl>
2023-01-18 11:44:04 +0100

    changed all text fonts

--- src/BedroomEndText.ts
+++ src/BedroomEndText.ts
@@ -8,60 +8,60 @@ export default class BedroomEndText extends Drawable {
 
   public textOne(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'pfffff…. What a terrible slee...', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, 'wait? What happened just now?', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'pfffff…. What a terrible slee...', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'wait? What happened just now?', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public textTwo(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'was it all just a dream?', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, 'The horse? The ads? The worms? ', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'was it all just a dream?', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'The horse? The ads? The worms? ', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public textThree(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'did all of that happen', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, 'because I downloaded the game?', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'did all of that happen', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'because I downloaded the game?', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public textFour(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'Timmy checks his phone to see if', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, 'the game he wanted is really released.', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'Timmy checks his phone to see if', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'the game he wanted is really released.', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
   }
 
   public textFive(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'The game… it is really out…', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, 'so was all of that actually real?', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'The game… it is really out…', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'so was all of that actually real?', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public textSix(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'I will be a good kid and', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, 'ask my parents for the game.', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'I will be a good kid and', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'ask my parents for the game.', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public textSeven(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'If they say no, I will study', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, 'harder so they will reward me :)', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'If they say no, I will study', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'harder so they will reward me :)', this.dimensionsX + 720, this.dimensionsY + 680, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public computerPrompt(canvas: HTMLCanvasElement, image: HTMLImageElement): void {
-    CanvasUtil.writeTextToCanvas(canvas, 'Press [SPACE] to open computer', this.dimensionsX + 50, this.dimensionsY + 740, 'left', 'arial', 40, 'white');
+    CanvasUtil.writeTextToCanvas(canvas, 'Press [SPACE] to open computer', this.dimensionsX + 50, this.dimensionsY + 740, 'left', 'DotGothic16', 40, 'white');
   }
 }
--- src/Whackamole/WhackamoleText.ts
+++ src/Whackamole/WhackamoleText.ts
@@ -8,39 +8,39 @@ export default class WhackamoleText extends Drawable {
 
   public textOne(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'You managed to defend against my ads….', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'You managed to defend against my ads….', this.dimensionsX + 720, this.dimensionsY + 660, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public textTwo(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 580);
-    CanvasUtil.writeTextToCanvas(canvas, 'Let us see how you defend against my worms…', this.dimensionsX + 720, this.dimensionsY + 650, 'center', 'arial', 20, 'black');
-    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'arial', 16, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'Let us see how you defend against my worms…', this.dimensionsX + 730, this.dimensionsY + 650, 'center', 'DotGothic16', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, '[SPACE] next', this.dimensionsX + 870, this.dimensionsY + 730, 'center', 'DotGothic16', 16, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 640);
   }
 
   public textThree(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 650);
-    CanvasUtil.writeTextToCanvas(canvas, 'Enough! I will destroy you myself!', this.dimensionsX + 720, this.dimensionsY + 730, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'Enough! I will destroy you myself!', this.dimensionsX + 720, this.dimensionsY + 730, 'center', 'DotGothic16', 20, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 710);
   }
 
   public textFour(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 650);
-    CanvasUtil.writeTextToCanvas(canvas, 'AAAAAARRRRRRRRGGGGHHHHHHHHH!!!!!', this.dimensionsX + 720, this.dimensionsY + 710, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'AAAAAARRRRRRRRGGGGHHHHHHHHH!!!!!', this.dimensionsX + 720, this.dimensionsY + 710, 'center', 'DotGothic16', 20, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 710);
   }
 
   public textFive(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 650);
-    CanvasUtil.writeTextToCanvas(canvas, 'You can not keep this up like this….', this.dimensionsX + 720, this.dimensionsY + 710, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'You can not keep this up like this….', this.dimensionsX + 720, this.dimensionsY + 710, 'center', 'DotGothic16', 20, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 710);
   }
 
   public textSix(canvas: HTMLCanvasElement, image: HTMLImageElement, talker: HTMLImageElement): void {
     CanvasUtil.drawImage(canvas, image, this.dimensionsX + 450, this.dimensionsY + 650);
-    CanvasUtil.writeTextToCanvas(canvas, 'You are doing better than I thought….', this.dimensionsX + 720, this.dimensionsY + 710, 'center', 'arial', 20, 'black');
+    CanvasUtil.writeTextToCanvas(canvas, 'You are doing better than I thought….', this.dimensionsX + 720, this.dimensionsY + 710, 'center', 'DotGothic16', 20, 'black');
     CanvasUtil.drawImage(canvas, talker, this.dimensionsX + 465, this.dimensionsY + 710);
   }
 
