5309350d8c815fb954f8e58c7d18b4f158a9ac22
ertu8820 <akta0002@hz.nl>
2023-01-18 15:57:57 +0100

    remove build

--- build/ArrowThrower/EnemyAD.js
+++
@@ -1,9 +0,0 @@
-import Drawable from '../Drawable.js';
-export default class EnemyAD extends Drawable {
-    constructor(maxY) {
-        super();
-        this.posX = this.dimensionsX;
-        this.posY = (Math.random() * ((maxY - 100) - this.dimensionsY) + this.dimensionsY);
-    }
-}
-//# sourceMappingURL=EnemyAD.js.map
--- build/app.js
+++
@@ -1,8 +0,0 @@
-import Nightmare from './Nightmare.js';
-import { GameLoop } from './GameLoop.js';
-const game = new Nightmare(document.getElementById('game'));
-const gameLoop = new GameLoop(game, GameLoop.PLAY_CATCH_UP);
-window.addEventListener('load', () => {
-    gameLoop.start();
-});
-//# sourceMappingURL=app.js.map
