2d437d5f719392a6e097a988bff0f3a2833bc6ab
ertu8820 <akta0002@hz.nl>
2023-01-20 10:34:06 +0100

    fixed heart image

--- src/ArrowThrower/HeartPowerup.ts
+++ src/ArrowThrower/HeartPowerup.ts
@@ -4,7 +4,7 @@ import Drawable from '../Drawable.js';
 export default class HeartPowerup extends Drawable {
   public constructor() {
     super();
-    this.image = CanvasUtil.loadNewImage('./placeholders/heart.png');
+    this.image = CanvasUtil.loadNewImage('./assets/heart.png');
     this.posX = this.dimensionsX;
     // eslint-disable-next-line max-len
     this.posY = (Math.random() * ((this.dimensionsY + this.backgroundHeight - 350) - this.dimensionsY + 100) + this.dimensionsY + 100);
